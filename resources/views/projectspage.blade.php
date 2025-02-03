6287e40a68db70a22c211de4f661b812a061a420
Gaurav Chaulagain <talktogaurav07@gmail.com>
2025-02-03 15:10:52 +0545

    + button

--- resources/views/projectspage.blade.php
+++ resources/views/projectspage.blade.php
@@ -163,7 +163,8 @@
             </div>
 
         </div>
-        <button class="bg-blue-500 text-white px-4 py-2 rounded-md mb-6" onclick="addFormSection()">+ Add</button>
+        <button class="bg-blue-500 text-white px-4 py-2 rounded-md mb-6" onclick="addFormSection()"><i
+                class="fa-solid fa-plus"></i></button>
         <script src="{{ asset('js/projectspage.js') }}" defer></script>
         <script>
             function deleteFormSection(button) {
